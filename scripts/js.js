a013b697b3cbf00fbad408d2201e35c404202246
yevstrikha <yevstrikha@gmail.om>
2022-07-12 13:59:19 -0400

    owl nav

--- scripts/js.js
+++ scripts/js.js
@@ -1,24 +1,56 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:30,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//
+//         },
+//         600:{
+//             items:3,
+//             nav:false,
+//             loop:true,
+//         },
+//         1000:{
+//             items:5,
+//             loop:true,
+//             nav:true,
+//         }
+//     }
+// })
+//
+//
+//
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:30,
     responsiveClass:true,
     responsive:{
         0:{
             items:1,
+            nav:true,
+            margin:10,
 
         },
         600:{
             items:3,
             nav:false,
-            loop:true,
+            margin:30,
+
         },
         1000:{
             items:5,
-            loop:true,
             nav:true,
+            loop:true,
+            margin:30,
+
+        },
+        1199:{
+            items:5,
+            nav:true,
+            loop:true,
+            margin:30,
+
         }
     }
 })
-
-
-
