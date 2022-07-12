2b21dc53d6bb8a37509b8f2cfd6dea735c0ec87e
yevstrikha <yevstrikha@gmail.om>
2022-07-11 20:27:41 -0400

    add animation(social)

--- scripts/js.js
+++ scripts/js.js
@@ -2,7 +2,6 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:30,
     responsiveClass:true,
-    nav:true,
     responsive:{
         0:{
             items:1,
@@ -11,10 +10,12 @@ $('.owl-carousel').owlCarousel({
         600:{
             items:3,
             nav:false,
+            loop:true,
         },
         1000:{
             items:5,
             loop:true,
+            nav:true,
         }
     }
 })
